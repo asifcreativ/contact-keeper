1a33bcccec6d83d3c8b57b59116e36db8faeff10
Muhammad Asif <asifcreativ@gmail.com>
2021-10-15 10:50:53 -0700

    node express initial setup

---
+++ NOTES.md
@@ -0,0 +1,6 @@
+npm i express bcryptjs jsonwebtoken config express-validator mongoose
+
+npm i -D nodemon concurrently
+
+npm run server
+url: http://localhost:5000
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  return res.send('Hello World');
+});
